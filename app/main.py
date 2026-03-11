6d73078d9026fcc4aa2e6566e207360d70ca77d6
minsu <soom990906@gmail.com>
2026-03-11 18:14:12 +0900

    feat) 행사 프로필 만들어주는 AI!

--- app/main.py
+++ app/main.py
@@ -1,7 +1,18 @@
 # app/main.py
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api import reco_router
+from app.api import image_router
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # 로컬 테스트용이므로 전부 허용
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(reco_router.router)
+app.include_router(image_router.router)
